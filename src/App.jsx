f850918c6c8903f581d3b20575a7635ae1974966
김병관 <ansd_my3073@naver.com>
2025-10-23 16:54:23 +0900

    feat: 에러 부분 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react'
 import './App.css'
 import WeatherBox from './component/WeatherBox.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import WeatherButton from "./component/WeatherButton.jsx";
 
 
